eada52ea7212bff5afef07b4f39d3d1571b5f8f0
dryhour <99828355+dryhour@users.noreply.github.com>
2026-02-24 12:30:07 -0500

    Initial commit

---
+++ src/main/java/com/example/music_catalog_api/MusicCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.music_catalog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/music_catalog_api/MusicCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.music_catalog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
